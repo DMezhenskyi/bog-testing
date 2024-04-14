a90138b286bc1fa2e65aecb80b21204bdabaee24
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2024-04-15 00:52:11 +0400

    solution: type coercions

--- projects/ui-kit/src/lib/utils/type-coercion.spec.ts
+++ projects/ui-kit/src/lib/utils/type-coercion.spec.ts
@@ -1,3 +1,34 @@
+import { isNumber, toBooleanProperty } from './type-coercion';
+
 describe('TypeCoercion Helper Function', () => {
-  it.todo('__YOUR_TEST_HERE');
+  describe(`isNumber Function`, () => {
+    it(`should treat integer 100 as a number`, () => {
+      const result = isNumber(100);
+      expect(result).toBe(true);
+    });
+    it(`should treat string '100' as a number`, () => {
+      const result = isNumber('100');
+      expect(result).toBe(true);
+    });
+    it(`should treat string '' as NOT a number`, () => {
+      expect(isNumber('')).toBe(false);
+    });
+    it(`should treat string '100abc' as NOT a number`, () => {
+      expect(isNumber('100abc')).toBe(false);
+    });
+    it(`should treat string {} as NOT a number`, () => {
+      expect(isNumber({})).toBe(false);
+    });
+  });
+  describe(`toBooleanProperty Function`, () => {
+    it(`should coerce 'false' to boolean false`, () => {
+      expect(toBooleanProperty('false')).toBe(false);
+    });
+    it(`should coerce '' to boolean true`, () => {
+      expect(toBooleanProperty('')).toBe(true);
+    });
+    it(`should coerce null to boolean false`, () => {
+      expect(toBooleanProperty(null)).toBe(false);
+    });
+  });
 });
