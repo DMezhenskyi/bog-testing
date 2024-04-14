0ffe68085b7c01160de9f73111c188fa475012c6
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2024-04-15 00:53:57 +0400

    solution: tests for trim string

--- projects/ui-kit/src/lib/utils/trim-str.spec.ts
+++ projects/ui-kit/src/lib/utils/trim-str.spec.ts
@@ -1,3 +1,15 @@
-describe('Trim String Helper Function', () => {
-  it.todo('__YOUR_TEST_HERE');
+import { trimSlashes } from './trim-str';
+
+describe('TrimStr Util Function', () => {
+  it('should trim slashes from the end of the string', () => {
+    expect(trimSlashes('https://test.com///')).toBe('https://test.com');
+  });
+  it('should trim slashes from the beginning of the string', () => {
+    expect(trimSlashes('//https://test.com')).toBe('https://test.com');
+  });
+  it('should throw an error if param is not a string', () => {
+    expect(() => trimSlashes(100)).toThrow(/invalid/i);
+    expect(() => trimSlashes({})).toThrow(/invalid/i);
+    expect(() => trimSlashes(null)).toThrow(/invalid/i);
+  });
 });
