1d897fbee8449d13bc68edf88a6576f0a31f79d0
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2024-04-15 20:52:26 +0400

    pipe: solution: Task 2 - Implement Test Cases

--- projects/ui-kit/src/lib/pipes/product-url/product-url.pipe.spec.ts
+++ projects/ui-kit/src/lib/pipes/product-url/product-url.pipe.spec.ts
@@ -1,11 +1,68 @@
+import { TestBed } from '@angular/core/testing';
+import { PRODUCT_BASE_URL, ProductUrlPipe } from './product-url.pipe';
+
 describe('ProductUrlPipe', () => {
+  it('should throw an error if base Url is not provided', () => {
+    // SETUP
+    TestBed.configureTestingModule({
+      providers: [
+        ProductUrlPipe,
+        {
+          provide: PRODUCT_BASE_URL,
+          useValue: '',
+        },
+      ],
+    });
+    // ACTION
+    const pipe = TestBed.inject(ProductUrlPipe);
 
-  it.todo('should throw an error if base Url is not provided');
+    // ASSERTION
+    expect(() => pipe.transform(1)).toThrow(/URL was not provided/i);
+  });
 
-  it.todo('should throw an error if id is not provided');
+  it('should throw an error if id is not provided', () => {
+    TestBed.configureTestingModule({
+      providers: [
+        ProductUrlPipe,
+        {
+          provide: PRODUCT_BASE_URL,
+          useValue: 'https://test.com',
+        },
+      ],
+    });
+    const pipe = TestBed.inject(ProductUrlPipe);
+    expect(() => pipe.transform('')).toThrow(/Invalid product id/i);
+  });
+
+  it('should return a valid product URL', () => {
+    TestBed.configureTestingModule({
+      providers: [
+        ProductUrlPipe,
+        {
+          provide: PRODUCT_BASE_URL,
+          useValue: 'https://test.com',
+        },
+      ],
+    });
+    const pipe = TestBed.inject(ProductUrlPipe);
+    const result = pipe.transform(1);
+    expect(result).toBe('https://test.com/product/1');
+  });
   
-  it.todo('should return a valid product URL');
-  
-  it.todo('should properly handle forward slashes');
+  it('should properly handle forward slashes', () => {
+    TestBed.configureTestingModule({
+      providers: [
+        ProductUrlPipe,
+        {
+          provide: PRODUCT_BASE_URL,
+          useValue: 'https://test.com/',
+        },
+      ],
+    });
+    const pipe = TestBed.inject(ProductUrlPipe);
 
+    const result = pipe.transform(1);
+
+    expect(result).toBe('https://test.com/product/1');
+  });
 });
