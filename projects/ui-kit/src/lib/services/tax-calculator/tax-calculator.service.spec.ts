9f2d095dd4518a6f7f4fcf1ec6e9633042164ad4
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2024-04-15 20:40:35 +0400

    Task 1 - Create Testing plan

--- projects/ui-kit/src/lib/services/tax-calculator/tax-calculator.service.spec.ts
+++ projects/ui-kit/src/lib/services/tax-calculator/tax-calculator.service.spec.ts
@@ -1,3 +1,14 @@
 describe('TaxCalculatorService', () => {
-  it.todo('__your_test_description__');
+
+  it.todo(`should properly calculate VAT for a given country`);
+  
+  it.todo(`should return 0 if isB2B flag is true`);
+  
+  describe(`TaxCalculatorService: Error Handling`, () => {
+    
+    it.todo(`should throw error if country isn't supported`);
+    
+    it.todo(`should throw error if price is negative number`);
+  
+  });
 });
