94c922765e5a604de929d70edf3a52255acd5e98
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2024-04-15 20:50:47 +0400

    pipe: solution: Task 1

--- projects/ui-kit/src/lib/pipes/product-url/product-url.pipe.spec.ts
+++ projects/ui-kit/src/lib/pipes/product-url/product-url.pipe.spec.ts
@@ -1,3 +1,11 @@
 describe('ProductUrlPipe', () => {
-  it.todo('__your_test_description__');
+
+  it.todo('should throw an error if base Url is not provided');
+
+  it.todo('should throw an error if id is not provided');
+  
+  it.todo('should return a valid product URL');
+  
+  it.todo('should properly handle forward slashes');
+
 });
