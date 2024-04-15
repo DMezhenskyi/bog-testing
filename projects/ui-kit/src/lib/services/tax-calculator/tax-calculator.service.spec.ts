270cfd992fcc764c066c51a178963e5831c90715
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2024-04-15 20:41:02 +0400

    ## Task 2 - Implement Test Cases

--- projects/ui-kit/src/lib/services/tax-calculator/tax-calculator.service.spec.ts
+++ projects/ui-kit/src/lib/services/tax-calculator/tax-calculator.service.spec.ts
@@ -1,14 +1,33 @@
-describe('TaxCalculatorService', () => {
+import { TaxCalculatorService } from './tax-calculator.service';
 
-  it.todo(`should properly calculate VAT for a given country`);
-  
-  it.todo(`should return 0 if isB2B flag is true`);
-  
+describe('TaxCalculatorService', () => {
+  let service: TaxCalculatorService;
+  beforeEach(() => {
+    // SETUP
+    service = new TaxCalculatorService();
+  });
+  it(`should properly calculate VAT for a given country`, () => {
+    // ACTION
+    const result = service.calculateVAT(100, 'ge');
+
+    // ASSERTION
+    expect(result).toBe(18);
+  });
+  it(`should return 0 if isB2B flag is true`, () => {
+    // ACTION
+    const result = service.calculateVAT(100, 'ge', true);
+    
+    // ASSERTION
+    expect(result).toBe(0);
+  });
   describe(`TaxCalculatorService: Error Handling`, () => {
-    
-    it.todo(`should throw error if country isn't supported`);
-    
-    it.todo(`should throw error if price is negative number`);
-  
+    it(`should throw error if country isn't supported`, () => {
+      expect(() => service.calculateVAT(100, 'ir')).toThrow(/isn't supported/i);
+    });
+    it(`should throw error if price is negative number`, () => {
+      expect(() => service.calculateVAT(-100, 'ge')).toThrow(
+        /can not be a negative number/i
+      );
+    });
   });
 });
