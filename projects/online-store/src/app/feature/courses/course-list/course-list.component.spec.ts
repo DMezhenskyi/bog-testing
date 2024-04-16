26d2bf08364b5e4ca27dd5024785049ca20c723d
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2024-04-16 19:52:13 +0400

    component: course-list: solution: Task 3 (implemented tests)

--- projects/online-store/src/app/feature/courses/course-list/course-list.component.spec.ts
+++ projects/online-store/src/app/feature/courses/course-list/course-list.component.spec.ts
@@ -1,20 +1,104 @@
 import { TestBed } from '@angular/core/testing';
 import { CourseListComponent } from './course-list.component';
 import { CoursesService } from '../courses.service';
+import { of, throwError } from 'rxjs';
+import { NetworkStatus } from '../../../core/models';
+import { By } from '@angular/platform-browser';
 import { Course } from '../courses.model';
 import { provideRouter } from '@angular/router';
 import { CartService } from '../../cart/cart.service';
 
 describe('CourseListComponent', () => {
-  
   it(`should show loading view if status loading`, () => {
-    const { fixture, debugEl, coursesService } = setup();
+    const { coursesService, fixture, debugEl } = setup();
+    jest.spyOn(coursesService, 'getCourses').mockReturnValue(
+      of<NetworkStatus>({
+        status: 'loading',
+      })
+    );
+    // initial change detection
+    fixture.detectChanges();
+    const loadingView = debugEl.query(
+      By.css('[data-testingId="loading-view"]')
+    );
+
+    expect(loadingView).not.toBeNull();
+    expect(loadingView.nativeElement.textContent).toContain('Loading');
   });
-  it.todo(`should show error view if courses could not be fetched`);
-  it.todo(`should render course cards if response successful`);
-  it.todo(`should add proper item to the cart`);
-  it.todo(`should have proper link to details page`);
-  
+  it(`should show error view if courses could not be fetched`, () => {
+    const { fixture, debugEl, coursesService } = setup();
+    jest.spyOn(coursesService, 'getCourses').mockReturnValue(
+      throwError(() => ({
+        status: 'error',
+        error: 'Internal Server Error',
+      }))
+    );
+    // initial change detection
+    fixture.detectChanges();
+
+    const errorView = debugEl.query(By.css('[data-testingId="error-view"]'));
+
+    expect(errorView).not.toBeNull();
+    expect(errorView.nativeElement.textContent).toContain('Could not load');
+  });
+  it(`should render course cards if response successful`, () => {
+    const { fixture, debugEl, testingCourses, coursesService } = setup();
+    jest.spyOn(coursesService, 'getCourses').mockReturnValue(
+      of({
+        status: 'success',
+        data: testingCourses,
+      })
+    );
+    fixture.detectChanges();
+
+    const cards = debugEl.queryAll(By.css('[data-testingId="card"]'));
+
+    expect(cards.length).toBe(2);
+    expect(cards[0].nativeElement.textContent).toContain(
+      testingCourses[0].name
+    );
+    expect(cards[1].nativeElement.textContent).toContain(
+      testingCourses[1].name
+    );
+  });
+  it(`should add proper item to the cart`, () => {
+    const { fixture, debugEl, cartService, testingCourses, coursesService } =
+      setup();
+    const courseToAdd = testingCourses[0];
+    jest.spyOn(coursesService, 'getCourses').mockReturnValue(
+      of({
+        status: 'success',
+        data: [courseToAdd],
+      })
+    );
+    fixture.detectChanges();
+
+    const cardAddButton = debugEl.query(
+      By.css('[data-testingId="add-to-cart"]')
+    );
+    cardAddButton.triggerEventHandler('click');
+
+    expect(cartService.add).toHaveBeenCalledWith(courseToAdd);
+  });
+  it(`should have proper link to details page`, () => {
+    const { fixture, debugEl, coursesService, testingCourses } = setup();
+    const courseToNavigate = testingCourses[0];
+    jest.spyOn(coursesService, 'getCourses').mockReturnValue(
+      of({
+        status: 'success',
+        data: [courseToNavigate],
+      })
+    );
+
+    fixture.detectChanges();
+    const detailsButton = debugEl.query(
+      By.css('[data-testingId="details-link"]')
+    );
+
+    expect(detailsButton.attributes['href']).toBe(
+      `/course/${courseToNavigate.id}`
+    );
+  });
 });
 
 function setup() {
@@ -63,3 +147,4 @@ function setup() {
 
   return { fixture, debugEl, coursesService, testingCourses, cartService };
 }
+
