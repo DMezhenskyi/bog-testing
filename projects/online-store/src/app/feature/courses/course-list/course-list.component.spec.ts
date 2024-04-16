3af95541bed7aa55697b6006232cad1111c2266f
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2024-04-16 19:48:02 +0400

    component: course-list: solution: Task 1

--- projects/online-store/src/app/feature/courses/course-list/course-list.component.spec.ts
+++ projects/online-store/src/app/feature/courses/course-list/course-list.component.spec.ts
@@ -1,3 +1,9 @@
 describe('CourseListComponent', () => {
-  it.todo('should test something');
+  
+  it.todo(`should show loading view if status loading`);
+  it.todo(`should show error view if courses could not be fetched`);
+  it.todo(`should render course cards if response successful`);
+  it.todo(`should add proper item to the cart`);
+  it.todo(`should have proper link to details page`);
+  
 });
