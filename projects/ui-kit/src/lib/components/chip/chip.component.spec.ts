320af63cc8802c6e74ead3a18d8375ebcf070999
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2024-04-16 19:42:31 +0400

    component: chip: solution: Task 3

--- projects/ui-kit/src/lib/components/chip/chip.component.spec.ts
+++ projects/ui-kit/src/lib/components/chip/chip.component.spec.ts
@@ -1,15 +1,35 @@
 import { TestBed } from "@angular/core/testing";
 import { ChipComponent } from "./chip.component";
+import { By } from "@angular/platform-browser";
 
 describe('ChipComponent', () => {
 
   it('should render properly the chip text', () => {
-    const {fixture, debugElement, componentRef} = setup();
+    // SETUP
+    const { debugElement, fixture, componentRef } = setup();
+    const testString = 'Angular Test';
+    
+    // ACTION
+    componentRef.setInput('text', testString);
+    fixture.detectChanges();
+
+    // ASSERTION
+    const chipText = debugElement.query(By.css('.chip-text'));
+    expect(chipText.nativeElement.textContent).toContain(testString);
   });
-  
-  it.todo('should render render the remove icon when removable is true');
-  
-  it.todo('should be focusable');
+  it('should render render the remove icon when removable is true', () => {
+    const { debugElement, fixture, componentRef } = setup();
+
+    componentRef.setInput('removable', true);
+    fixture.detectChanges();
+
+    const removeIcon = debugElement.query(By.css('.chip-remove-icon'));
+    expect(removeIcon).toBeTruthy();
+  });
+  it('should be focusable', () => {
+    const { debugElement } = setup();
+    expect(debugElement.nativeElement.tabIndex).toBe(0);
+  });
   
   it.todo(
     'when removed is clicked, should emit the removed event with the chip instance'
