43b0178a6d37a8e5b5738d21cfbef2ec38c57324
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2024-04-16 19:42:00 +0400

    component: chip: solution: Task 2 (testbed setup)

--- projects/ui-kit/src/lib/components/chip/chip.component.spec.ts
+++ projects/ui-kit/src/lib/components/chip/chip.component.spec.ts
@@ -1,6 +1,11 @@
+import { TestBed } from "@angular/core/testing";
+import { ChipComponent } from "./chip.component";
+
 describe('ChipComponent', () => {
 
-  it.todo('should render properly the chip text');
+  it('should render properly the chip text', () => {
+    const {fixture, debugElement, componentRef} = setup();
+  });
   
   it.todo('should render render the remove icon when removable is true');
   
@@ -10,3 +15,13 @@ describe('ChipComponent', () => {
     'when removed is clicked, should emit the removed event with the chip instance'
   );
 });
+
+function setup() {
+  const fixture = TestBed.createComponent(ChipComponent);
+  const componentRef = fixture.componentRef;
+  const debugElement = fixture.debugElement;
+
+  fixture.detectChanges();
+
+  return { fixture, componentRef, debugElement };
+}
