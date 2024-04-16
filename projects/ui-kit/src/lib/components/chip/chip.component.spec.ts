684b7d496c9c461cbf35337d51b6b2c52f2f8946
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2024-04-16 19:41:09 +0400

    component: chip: solution: Task 1

--- projects/ui-kit/src/lib/components/chip/chip.component.spec.ts
+++ projects/ui-kit/src/lib/components/chip/chip.component.spec.ts
@@ -1,3 +1,12 @@
-describe('CanDisableDirective', () => {
-  it.todo('Test placeholder');
+describe('ChipComponent', () => {
+
+  it.todo('should render properly the chip text');
+  
+  it.todo('should render render the remove icon when removable is true');
+  
+  it.todo('should be focusable');
+  
+  it.todo(
+    'when removed is clicked, should emit the removed event with the chip instance'
+  );
 });
