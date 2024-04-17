e1b654c32d024247910bc80d612f867f4c48c0a9
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2024-04-17 19:35:17 +0400

    IMPL: Show/hide error message if empty email

--- projects/online-store/src/app/feature/signin/signin.component.ts
+++ projects/online-store/src/app/feature/signin/signin.component.ts
@@ -1,16 +1,18 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   template: `
-    <form>
-      <input [(ngModel)]="email" name="email" placeholder="Enter Email" type="email" data-testId="email">
+    <form>  
+      <input required #emailField="ngModel" [(ngModel)]="email" name="email" placeholder="Enter Email" type="email" data-testId="email">
+      <div *ngIf="emailField.hasError('required')" data-testId="email-required">This field is required</div>
       <input [(ngModel)]="password" name="password" placeholder="Enter Your Password" type="password" data-testId="password">
       <button data-testId="signin-button">SignIn</button>
     </form>
   `,
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
 })
 export default class SignInComponent {
   email = '';
