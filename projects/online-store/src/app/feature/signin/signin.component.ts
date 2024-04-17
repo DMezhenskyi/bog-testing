da0ed480081c0577755c9766458009d7a1e2d78e
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2024-04-17 19:44:19 +0400

    IMPL: should disable form button if form is invalid

--- projects/online-store/src/app/feature/signin/signin.component.ts
+++ projects/online-store/src/app/feature/signin/signin.component.ts
@@ -4,12 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 @Component({
   template: `
-    <form>  
+    <form #form="ngForm">  
       <input required email #emailField="ngModel" [(ngModel)]="email" name="email" placeholder="Enter Email" type="email" data-testId="email">
       <div *ngIf="emailField.hasError('required')" data-testId="email-required">This field is required</div>
       <div *ngIf="emailField.hasError('email')" data-testId="email-invalid">This email is invalid</div>
       <input [(ngModel)]="password" name="password" placeholder="Enter Your Password" type="password" data-testId="password">
-      <button data-testId="signin-button">SignIn</button>
+      <button [disabled]="!form.valid" data-testId="signin-button">SignIn</button>
     </form>
   `,
   standalone: true,
