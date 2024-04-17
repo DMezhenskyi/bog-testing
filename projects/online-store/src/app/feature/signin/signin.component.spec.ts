cd78640ef1936cc0f978422cf0a66bdcba655902
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2024-04-17 18:49:30 +0400

    REFACTOR: extract tests in appearance test suit

--- projects/online-store/src/app/feature/signin/signin.component.spec.ts
+++ projects/online-store/src/app/feature/signin/signin.component.spec.ts
@@ -3,35 +3,37 @@ import SignInComponent from "./signin.component"
 import { By } from "@angular/platform-browser";
 
 describe('SigninComponent', () => {
-  it('should render email & password input fields', () => {
-    const { getFormElements} = setup();
-    
-    const {emailField, passworField} = getFormElements()
-    
-    expect(emailField).toBeTruthy();
-    expect(passworField).toBeTruthy();
-  })
-  it('should verify that email & password have proper attributes', () => {
-    const {getFormElements} = setup();
-    
-    const {emailField, passworField} = getFormElements();
-
-    expect(emailField.nativeElement.type).toBe('email');
-    expect(passworField.nativeElement.type).toBe('password');
-  })
-  it('form controls should have placeholders', () => {
-    const {getFormElements} = setup();
-    const {emailField, passworField} = getFormElements();
-    
-    expect(emailField.nativeElement.placeholder).toMatch(/email/i)
-    expect(passworField.nativeElement.placeholder).toMatch(/password/i)
-  })
-  it('should render a submit button', () => {
-    const {getFormElements} = setup();
-    const {button} = getFormElements();
-
-    expect(button).toBeTruthy();
-    expect(button.nativeElement.textContent).toMatch(/signin/i)
+  describe('Form Appearance', () => {
+    it('should render email & password input fields', () => {
+      const { getFormElements} = setup();
+      
+      const {emailField, passworField} = getFormElements()
+      
+      expect(emailField).toBeTruthy();
+      expect(passworField).toBeTruthy();
+    })
+    it('should verify that email & password have proper attributes', () => {
+      const {getFormElements} = setup();
+      
+      const {emailField, passworField} = getFormElements();
+  
+      expect(emailField.nativeElement.type).toBe('email');
+      expect(passworField.nativeElement.type).toBe('password');
+    })
+    it('form controls should have placeholders', () => {
+      const {getFormElements} = setup();
+      const {emailField, passworField} = getFormElements();
+      
+      expect(emailField.nativeElement.placeholder).toMatch(/email/i)
+      expect(passworField.nativeElement.placeholder).toMatch(/password/i)
+    })
+    it('should render a submit button', () => {
+      const {getFormElements} = setup();
+      const {button} = getFormElements();
+  
+      expect(button).toBeTruthy();
+      expect(button.nativeElement.textContent).toMatch(/signin/i)
+    })
   })
 })
 
