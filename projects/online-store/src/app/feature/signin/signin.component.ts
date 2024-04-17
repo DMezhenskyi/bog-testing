f6f38ca4802216d5d71329696f688998eb86fad1
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2024-04-17 19:40:11 +0400

    IMPL: Show/hide error message if invalid email

--- projects/online-store/src/app/feature/signin/signin.component.ts
+++ projects/online-store/src/app/feature/signin/signin.component.ts
@@ -5,8 +5,9 @@ import { FormsModule } from '@angular/forms';
 @Component({
   template: `
     <form>  
-      <input required #emailField="ngModel" [(ngModel)]="email" name="email" placeholder="Enter Email" type="email" data-testId="email">
+      <input required email #emailField="ngModel" [(ngModel)]="email" name="email" placeholder="Enter Email" type="email" data-testId="email">
       <div *ngIf="emailField.hasError('required')" data-testId="email-required">This field is required</div>
+      <div *ngIf="emailField.hasError('email')" data-testId="email-invalid">This email is invalid</div>
       <input [(ngModel)]="password" name="password" placeholder="Enter Your Password" type="password" data-testId="password">
       <button data-testId="signin-button">SignIn</button>
     </form>
