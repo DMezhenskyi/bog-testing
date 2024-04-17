bbe409fddcd09106c8a74da986788b7e87e63964
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2024-04-17 19:50:05 +0400

    REFACTOR: Migrate to Reactive Forms

--- projects/online-store/src/app/feature/signin/signin.component.ts
+++ projects/online-store/src/app/feature/signin/signin.component.ts
@@ -1,21 +1,23 @@
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   template: `
-    <form #form="ngForm">  
-      <input required email #emailField="ngModel" [(ngModel)]="email" name="email" placeholder="Enter Email" type="email" data-testId="email">
-      <div *ngIf="emailField.hasError('required')" data-testId="email-required">This field is required</div>
-      <div *ngIf="emailField.hasError('email')" data-testId="email-invalid">This email is invalid</div>
-      <input [(ngModel)]="password" name="password" placeholder="Enter Your Password" type="password" data-testId="password">
+    <form [formGroup]="form">  
+      <input formControlName="email" name="email" placeholder="Enter Email" type="email" data-testId="email">
+      <div *ngIf="form.get('email')?.getError('required')" data-testId="email-required">This field is required</div>
+      <div *ngIf="form.get('email')?.getError('email')" data-testId="email-invalid">This email is invalid</div>
+      <input placeholder="Enter Your Password" type="password" data-testId="password">
       <button [disabled]="!form.valid" data-testId="signin-button">SignIn</button>
     </form>
   `,
   standalone: true,
-  imports: [FormsModule, NgIf],
+  imports: [ReactiveFormsModule, NgIf],
 })
 export default class SignInComponent {
-  email = '';
-  password = '';
+ form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('')
+ })
 }
