ea16741efaa9062c25cc16a187cc456637a736df
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2024-04-17 18:18:59 +0400

    TEST: render email and password

--- projects/online-store/src/app/feature/signin/signin.component.spec.ts
+++ projects/online-store/src/app/feature/signin/signin.component.spec.ts
@@ -1,8 +1,17 @@
 import { TestBed } from "@angular/core/testing"
 import SignInComponent from "./signin.component"
+import { By } from "@angular/platform-browser";
 
 describe('SigninComponent', () => {
+  it('should render email & password input fields', () => {
+    const {debugEl} = setup();
 
+    const emailField = debugEl.query(By.css('[data-testId="email"]'));
+    const passworField = debugEl.query(By.css('[data-testId="password"]'));
+
+    expect(emailField).toBeTruthy();
+    expect(passworField).toBeTruthy();
+  })
 })
 
 function setup() {
